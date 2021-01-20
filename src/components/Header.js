6d474afc4e3bcf37a18a64df74ccca7c3ce7185c
Eugene Kriulin <eakriulin@gmail.com>
2021-01-20 18:59:27 +0300

    implemented burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,85 +1,96 @@
+import React, { useState } from 'react';
 import '../styles/Header.scss';
 import logo from '../images/logo.svg';
 import phone from '../images/phone.svg';
 import pin from '../images/map-pin.svg';
 import briefcase from '../images/briefcase.svg';
+import DropDownMenu from './common/DropDownMenu';
 
-const Header = () => (
-    <div className="header-section">
-        <div className="primary-container">
-            <div className="wrapper">
-                <div className="primary">
-                    <div className="header-left-group">
-                        <div className="burger-menu">
-                            <div></div>
-                            <div></div>
-                            <div></div>
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    const getMenuCSSClass = () => isMenuOpen ? 'burger-menu-close' : 'burger-menu-open';
+
+    return (
+        <div className="header-section">
+            <div className="primary-container">
+                <div className="wrapper">
+                    <div className="primary">
+                        <div className="header-left-group">
+                            <a className="link" href={isMenuOpen ? '#menu' : '/#'}>
+                                <div className={getMenuCSSClass()} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </a>
+                            <img id="logo" alt="Woodstone Online" src={logo}></img>
+                            <span className="descriptor green-text">Начало загородной жизни</span>
                         </div>
-                        <img id="logo" alt="Woodstone Online" src={logo}></img>
-                        <span className="descriptor green-text">Начало загородной жизни</span>
-                    </div>
-                    <div className="header-center-group">
-                        <a className="menu-link" href="#services">
-                            <div className="menu-link-area">
-                                <span className="menu-link-text">Услуги</span>
-                            </div>
-                        </a>
-                        <a className="menu-link" href="#gallery">
-                            <div className="menu-link-area">
-                                <span className="menu-link-text">Галерея</span>
-                            </div>
-                        </a>
-                        <a className="menu-link" href="#technologies">
-                            <div className="menu-link-area">
-                                <span className="menu-link-text">Технологии</span>
-                            </div>
-                        </a>
-                        <a className="menu-link" href="#partners">
-                            <div className="menu-link-area">
-                                <span className="menu-link-text">Партнеры</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="header-right-group">
-                        <a href="tel:+74955488594">
-                            <img id="phone-icon" src={phone} alt=""></img>
-                        </a>
+                        <div className="header-center-group">
+                            <a className="menu-link" href="#services">
+                                <div className="menu-link-area">
+                                    <span className="menu-link-text">Услуги</span>
+                                </div>
+                            </a>
+                            <a className="menu-link" href="#gallery">
+                                <div className="menu-link-area">
+                                    <span className="menu-link-text">Галерея</span>
+                                </div>
+                            </a>
+                            <a className="menu-link" href="#technologies">
+                                <div className="menu-link-area">
+                                    <span className="menu-link-text">Технологии</span>
+                                </div>
+                            </a>
+                            <a className="menu-link" href="#partners">
+                                <div className="menu-link-area">
+                                    <span className="menu-link-text">Партнеры</span>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="header-right-group">
+                            <a href="tel:+74955488594">
+                                <img id="phone-icon" src={phone} alt=""></img>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="secondary-container">
-            <div className="wrapper">
-                <div className="secondary">
-                    <div className="information">
-                        <div className="location">
-                            <img className="information-icon" src={pin} alt=""></img>
-                            <span className="information-title">Екатеринбург</span>
-                        </div>
-                        <a className="information-link" href="#partnership">
-                            <div className="partners">
+            <DropDownMenu isOpen={isMenuOpen} />
+            <div className="secondary-container">
+                <div className="wrapper">
+                    <div className="secondary">
+                        <div className="information">
+                            <div className="location">
+                                <img className="information-icon" src={pin} alt=""></img>
+                                <span className="information-title">Екатеринбург</span>
+                            </div>
+                            <a className="information-link" href="#partnership">
+                                <div className="partners">
+                                        <img className="information-icon" src={briefcase} alt=""></img>
+                                        <span className="information-title">Партнерам</span>
+                                </div>
+                            </a>
+                            <a className="information-link" href="#office">
+                                <div className="office">
                                     <img className="information-icon" src={briefcase} alt=""></img>
-                                    <span className="information-title">Партнерам</span>
-                            </div>
-                        </a>
-                        <a className="information-link" href="#office">
-                            <div className="office">
-                                <img className="information-icon" src={briefcase} alt=""></img>
-                                <span className="information-title">Работа офиса</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="contacts">
-                        <div className="number-and-label">
-                            <span className="number">+7 (495) 548-85-94</span>
-                            <div className="label"><span className="label-text">Сейчас работаем</span></div>
+                                    <span className="information-title">Работа офиса</span>
+                                </div>
+                            </a>
                         </div>
-                        <a className="callback-button" href="#callback">Перезвоните мне</a>
+                        <div className="contacts">
+                            <div className="number-and-label">
+                                <span className="number">+7 (495) 548-85-94</span>
+                                <div className="label"><span className="label-text">Сейчас работаем</span></div>
+                            </div>
+                            <a className="callback-button" href="#callback">Перезвоните мне</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default Header;
---
+++ src/components/common/DropDownMenu.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../../styles/DropDownMenu.scss';
+import pin from '../../images/map-pin.svg';
+import clock from '../../images/clock.svg';
+import facebook from '../../images/facebook.svg';
+import vk from '../../images/vk.svg';
+import instagram from '../../images/instagram.svg';
+import youtube from '../../images/youtube.svg';
+
+export const DropDownMenu = ({ isOpen }) => {
+    return (
+            <div id="menu" className="drop-down-menu">
+                <div className="container">
+                    <div className="content">
+                        <div className="wrapper">
+                            <div className="address-block">
+                                    <div className="content-block-title">
+                                        <img className="content-icon pin" src={pin} alt=""></img>
+                                        <p className="content-subtitle">г. Екатеринбург</p>
+                                    </div>
+                                    <p className="content-text">ул. Малышева, 51, офис 2404</p>
+                            </div>
+                            <div className="timetable-block">
+                                <div className="content-block-title">
+                                    <img className="content-icon clock" src={clock} alt=""></img>
+                                    <p className="content-subtitle">Время работы офиса</p>
+                                </div>
+                                <div className="timetable">
+                                    <p className="content-text">Пн. – Пт.:<br/>Сб. – Вс.:</p>
+                                    <p className="content-text">с 9:00 до 19:00<br/>Выходной</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="social">
+                        <div className="wrapper">
+                            <div className="social-icons-block">
+                                <a href="https://www.facebook.com/woodstoneekb/" className="social-icon" target="_blank" rel="noopener noreferrer"><img src={facebook} alt=""></img></a>
+                                <a href="https://www.facebook.com/woodstoneekb/" className="social-icon" target="_blank" rel="noopener noreferrer"><img src={vk} alt=""></img></a>
+                                <a href="https://www.instagram.com/woodstone_ekb/" className="social-icon" target="_blank" rel="noopener noreferrer"><img src={instagram} alt=""></img></a>
+                                <a href="https://youtu.be/-PvLnOFiFLU" className="social-icon" target="_blank" rel="noopener noreferrer"><img src={youtube} alt=""></img></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+};
+
+export default DropDownMenu;
