2c8f8ff05a41ad375cc7e08323003434fe35ad37
Eugene Kriulin <eakriulin@gmail.com>
2021-01-22 16:48:42 +0300

    updated home gallery pictures, cleared unused assets

--- src/components/HousesGallery.js
+++ src/components/HousesGallery.js
@@ -1,34 +1,22 @@
 import '../styles/HousesGallery.scss';
 import ScrollContainer from 'react-indiana-drag-scroll';
-import picture from '../images/gallery-1.png';
+import picture1 from '../images/housef-1.png';
+import picture2 from '../images/housef-2.png';
+import picture3 from '../images/housef-3.png';
+import picture4 from '../images/housef-4.png';
+import picture5 from '../images/housef-5.png';
+import picture6 from '../images/housef-6.png';
+import picture7 from '../images/housef-7.png';
+import picture8 from '../images/housef-8.png';
 
 const HousesGallery = () => (
     <div className="gallery-section">
         <ScrollContainer className="houses-gallery">
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
-            <div className="gallery-item">
-                <img src={picture} className="picture" alt="" />
-            </div>
+            {[picture1, picture2, picture3, picture5, picture4, picture6, picture7, picture8].map((picture, idx) => (
+                <div className="gallery-item">
+                    <img key={idx} src={picture} className="picture" alt="" />
+                </div>
+            ))}
         </ScrollContainer>
     </div>
 );
